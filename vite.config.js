a975df80e7e8318d38d4bbd3e38044abe00c633d
Eze <Riccaesteban@gmail.com>
2024-06-05 04:14:19 -0300

    modifique nuevamente el vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://Eze-Ricca.github.io", // Reemplaza <TU_USUARIO> y <REPO> con tu información específica
+  base: "/route-state/",
 });
