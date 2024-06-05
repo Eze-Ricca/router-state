735e8e60598e12bcfc1eab8b82ccfe6e7da5f3c6
Eze <Riccaesteban@gmail.com>
2024-06-05 14:17:13 -0300

    nuevo build

--- src/pages/thanyou.ts
+++ src/pages/thanyou.ts
@@ -8,13 +8,13 @@ export function initThankyou(params: {
   const newState = state.getState();
 
   div.innerHTML = /*html*/ `
-    <title-el style="text-align:center">Gracias ${newState.nombre}</title-el>
+    <title-el style="text-align:center">Gracias ${newState.nombre}!</title-el>
     <subtitle-el>Toda la informacion que nos brindaste es muy importante</subtitle-el>
     <br>
     <br>
     <p>Email: <strong>${newState.email}</strong></p>
-    <p>Bebida: <strong>${newState.bebida}</strong></p>
-    <p>Comida:<strong>${newState.comida}</strong></p>
+    <p>Bebida: <strong> ${newState.bebida}</strong></p>
+    <p>Comida:<strong> ${newState.comida}</strong></p>
     
     <button-el id="thankyou" contenido="De nada" ConColor><button-el>
     `;
